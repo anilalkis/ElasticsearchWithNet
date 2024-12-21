f6c689bf9a09187e5d9fd059c03b85388fb1fc00
Anil Alkis <anilalkis89@gmail.com>
2024-12-21 17:25:12 +0300

    BaseController

---
+++ API/Elasticsearch/Elasticsearch.API/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using Elasticsearch.API.Dtos.ProductDtos;
+using Elasticsearch.API.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Elasticsearch.API.Controllers
+{
+    public class ProductsController : BaseController
+    {
+        private readonly ProductService _productService;
+        public ProductsController(ProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(ProductCreateDto request)
+        {
+            
+            return CreateActionResult(await _productService.SaveAsync(request));
+        }
+    }
+}
